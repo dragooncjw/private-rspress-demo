023ff30a384d245950d097b925efd23c039d65d3
chenjiawei.inizio <chenjiawei.inizio@bytedance.com>
2025-01-08 15:21:01 +0800

    chore: save

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: '/private-rspress-demo/',
+  base: '/',
   title: 'Cjw Demo Site',
   icon: '/rspress-icon.png',
   logo: {
